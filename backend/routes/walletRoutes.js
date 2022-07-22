1f64ae774a2916e901af254a02bd7628c92644f1
KC12477 <91883159+KC12477@users.noreply.github.com>
2022-07-22 12:27:04 +0800

    Template methods for transaction and wallet

--- backend/routes/walletRoutes.js
+++ backend/routes/walletRoutes.js
@@ -1,7 +1,13 @@
 import express from "express";
 import { check } from "express-validator";
+import { getWallets } from "../controllers/walletController.js"
+import { deleteWallet } from "../controllers/walletController.js"
 
 // Set up the router
 const router = express.Router();
+
+walletRoutes.get('/', getWallets)
+
+walletRoutes.delete('/', deleteWallet)
 
 export const walletRoutes = router;
